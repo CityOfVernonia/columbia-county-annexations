17397f4caf692e905f807146ba671c88d1da8ab1
City of Vernonia GIS <gis@vernonia-or.gov>
2021-12-02 10:58:00 -0800

    go!

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# vite-map-app
+# vernonia-annexations
 
-City of Vernonia web map app template.
+Web map application for viewing Vernonia annexations and related documents.
+
+https://vernonia-annexations.netlify.app/
+
+***
+
+Made with :heart: and :coffee: in Vernonia, Oregon
--- src/app.ts
+++ src/app.ts
@@ -4,27 +4,115 @@ import './main.scss';
 import esriConfig from '@arcgis/core/config';
 
 // loading screen
-import LoadingScreen from '@vernonia/core/widgets/LoadingScreen';
+import LoadingScreen from './core/widgets/LoadingScreen';
+import DisclaimerModal from './core/widgets/DisclaimerModal';
 
 // map, view and layers
 import Map from '@arcgis/core/Map';
 import MapView from '@arcgis/core/views/MapView';
 import Basemap from '@arcgis/core/Basemap';
+import FeatureLayer from '@arcgis/core/layers/FeatureLayer';
+import GeoJSONLayer from '@arcgis/core/layers/GeoJSONLayer';
+import { SimpleRenderer } from '@arcgis/core/renderers';
+import { SimpleFillSymbol, TextSymbol } from '@arcgis/core/symbols';
+import PopupTemplate from '@arcgis/core/PopupTemplate';
+import LabelClass from '@arcgis/core/layers/support/LabelClass';
+
+import { DateTime } from 'luxon';
 
 // layout
-import Viewer from '@vernonia/core/layouts/Viewer';
+import Viewer from './core/layouts/Viewer';
 
 // widgets
-import ViewControl from '@vernonia/core/widgets/ViewControl';
+import ViewControl from './core/widgets/ViewControl';
+import UIWidgetSwitcher from './core/widgets/UIWidgetSwitcher';
+import LayerListLegend from './core/widgets/LayerListLegend';
 
 // config portal and auth
 esriConfig.portalUrl = 'https://gisportal.vernonia-or.gov/portal';
 
 // app config and init loading screen
-const title = 'Vite Map App';
+const title = 'Vernonia Annexations';
 
 const loadingScreen = new LoadingScreen({
   title,
+});
+
+if (!DisclaimerModal.isAccepted()) {
+  new DisclaimerModal({});
+}
+
+const annexations = new GeoJSONLayer({
+  url: 'https://vernonia-annexation-documents.netlify.app/vernonia-annexations.geojson',
+  outFields: ['*'],
+  title: 'Annexations',
+  renderer: new SimpleRenderer({
+    symbol: new SimpleFillSymbol({
+      color: [255, 0, 0, 0.1],
+      outline: {
+        style: 'solid',
+        color: 'red',
+        width: 1,
+      },
+    }),
+  }),
+  labelingInfo: [
+    new LabelClass({
+      symbol: new TextSymbol({
+        color: 'red',
+        haloColor: 'white',
+        haloSize: 1,
+        font: {
+          size: 7
+        },
+      }),
+      labelPlacement: 'above-center',
+      labelExpressionInfo: {
+        expression: '$feature.GIS_IMAGE + TextFormatting.NewLine + ISOMonth(Date($feature.DateComple)) + "/" + Day(Date($feature.DateComple)) + "/" + ISOYear(Date($feature.DateComple))',
+      }
+    }),
+  ],
+  popupTemplate: new PopupTemplate({
+    title: 'Annexation',
+    content: (event: any) => {
+      const {
+        graphic: { attributes },
+      } = event;
+
+      const CSS = {
+        table: 'esri-widget__table',
+        th: 'esri-feature__field-header',
+        td: 'esri-feature__field-data',
+      };
+
+      return (document.createElement('div').innerHTML = `
+        <table class="${CSS.table}">
+          <tr>
+            <th class="${CSS.th}">Date</th>
+            <td class="${CSS.td}">${DateTime.fromMillis(attributes.DateComple).toLocaleString(DateTime.DATE_SHORT)}</td>
+          </tr>
+          <tr>
+            <th class="${CSS.th}">Documents</th>
+            <td class="${CSS.td}">
+              <a href="https://vernonia-annexation-documents.netlify.app/docs/${attributes.GIS_IMAGE}.pdf" target="_blank">View ${attributes.GIS_IMAGE}.pdf</a>
+            </td>
+          </tr>
+        </table>
+      `);
+    },
+  }),
+});
+
+const cityLimits = new FeatureLayer({
+  portalItem: {
+    id: 'eb0c7507611e44b7923dd1c0167e3b92',
+  },
+});
+
+const ugb = new FeatureLayer({
+  portalItem: {
+    id: '2f760ba990ab4d6e831d04b85a8a0bf3',
+  },
 });
 
 // view
@@ -35,7 +123,7 @@ const view = new MapView({
         id: 'f36cd213cc934d2391f58f389fc9eaec',
       },
     }),
-    layers: [],
+    layers: [annexations, ugb, cityLimits],
     ground: 'world-elevation',
   }),
   zoom: 15,
@@ -55,10 +143,27 @@ const view = new MapView({
 new Viewer({
   view,
   title,
+  includeSearch: false,
 });
 
 view.when(() => {
   view.ui.add(new ViewControl({ view }), 'top-left');
+
+  view.ui.add(
+    new UIWidgetSwitcher({
+      widgetInfos: [
+        {
+          widget: new LayerListLegend({
+            view,
+            addFromWeb: false,
+          }),
+          text: 'Layers',
+          icon: 'layers',
+        }
+      ]
+    }),
+    'top-right',
+  );
 
   loadingScreen.end();
 });
